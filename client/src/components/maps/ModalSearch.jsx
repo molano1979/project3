22902d403437adbe3a5015cdbd156b842064f361
Unknown <danila.vpopov@gmail.com>
2022-05-24 19:41:46 -0700

    modals for the map seqarch

---
+++ client/src/components/maps/ModalSearch.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+const index = () => {
+  return (
+    <section className="main-content ">
+          <div className="col-xl-6 col-lg-5 col-12">
+            <div className="row">
+              <div className="col-12">
+              <hr className="separator mt-1" />
+                <h3 className="text-uppercase custom-title mb-0 ft-wt-600">
+                  Search for segments
+                </h3>
+              </div>
+              <div className="col-12 d-block d-sm-none">
+              <div className="col-12 mt-1">
+                <a className="button" href="#">
+                  <button >Submit</button>
+                  <span className="button-icon fa fa-download"></span>
+                </a>
+              </div>
+        
+            </div>
+          </div>
+        </div>
+          <hr className="separator mt-1" />
+    </section>
+  );
+};
+
+export default index;
